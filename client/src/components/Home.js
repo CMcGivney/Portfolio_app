8f425a1a03c29d469e50c213bd8f31b8dc5df4c0
Chris McGivney <cmmcgivney84@gmail.com>
2019-08-08 09:57:56 -0600

    skills layout complete, animate next

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Header, Image, } from 'semantic-ui-react';
+import React from 'react'
+import { Header, Image, } from 'semantic-ui-react'
 import Alaska from '../images/Alaska.jpg'
+
 const Home = () => (
   <>
   <Header as="h1" textAlign="center">Welcome</Header>
@@ -8,13 +9,14 @@ const Home = () => (
   <Image 
   src={Alaska} 
   alt="Alaska"
-  height="500"
-  width="1000"
+  size="large"
+  centered
   />
   <Header as="h3">A little about me:</Header>
   <p>I live in Park City, Utah. I have called Utah home for the past 15 years. I grew up in California, and was recruited from San Francisco to move here to open/run restaurants, a career that at the time I was very passionate about. </p>
-  <p>I am a new father, which is kind of how I got into coding and web development. I was the day shift with the kiddo and with lots of time on my hands I started down the road of coding. FreeCodeCamp here and StackOverflow there until I finally commited to a Full Stack Program through the University Of Utah at Dev Point Labs.</p>
+  <p>I am a new father, which is kind of how I got into coding and web development. I was the day shift with the kiddo and with lots of time on my hands I started down the road of coding. FreeCodeCamp here and StackOverflow there until I finally decided to deep dive into a Full Stack Program through the University Of Utah at Dev Point Labs.</p>
   <p>I love my new career path, and have a new passion for all that goes into software and web development. I know I am very green in this business, but I believe that with hard work and a strong curiosity I can have a successful life in this ever changing Tech landscape. </p>
+  <hr />
   <footer>this will be the footer</footer>
   </>
 )
--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,8 +1,88 @@
 import React from 'react';
-import { Header, } from 'semantic-ui-react';
+import { Header, Grid, Image } from 'semantic-ui-react';
+import CSS3 from '../images/CSS3.png'
+import ES6 from '../images/ES6.png'
+import GitHub from '../images/GitHub.png'
+import HTML5 from '../images/HTML5.png'
+import jquery from '../images/jquery.png'
+import JS from '../images/JS.png'
+import Postgresql from '../images/Postgresql.jpeg'
+import Rails from '../images/Rails.png'
+import ReactIcon from '../images/React.png'
+import Ruby from '../images/Ruby.jpeg'
+import SemanticR from '../images/semanticR.png'
+import SQL from '../images/Sql.png'
+import Sqlectron from '../images/sqlectron.png'
+import BootStrap from '../images/BootStrap.png'
+import BootsReact from '../images/BootstrapReact.png'
+import Semantic from '../images/Semantic.png'
+
 
 const Skills = () => (
-  <Header as="h3" textAlign="center">Skills</Header>
+  <>
+  <Header as="h1" textAlign="center">Skills</Header>
+  <Header as="p" textAlign="center">"That One Day Will Pay The Bills"</Header>
+  <hr/>
+  <Grid padding="5px" margintop="25px">
+    <Grid.Row columns={4}>
+      <Grid.Column>
+        
+        <Image src={HTML5} alt="HTML5" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={CSS3} alt="CSS3" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={JS} alt="JavaScript" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={jquery} alt="JQuery" size="small" />
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row columns={4}>
+      <Grid.Column>
+        <Image src={Ruby} alt="Ruby" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={Rails} alt="Rails" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={Postgresql} alt="Postgresql" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={ES6} alt="ES6" size="small" />
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row columns={4}>
+      <Grid.Column>
+        <Image src={ReactIcon} alt="React Icon" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={SemanticR} alt="Semantic-ui-react" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={Sqlectron} alt="Sqlectron" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={SQL} alt="SQL" size="small" />
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row columns={4}>
+      <Grid.Column>
+        <Image src={BootStrap} alt="Bootstrap" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={BootsReact} alt="" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={Semantic} alt="" size="small" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={GitHub} alt="GitHub" size="small" />
+      </Grid.Column>
+    </Grid.Row>
+    </Grid>
+</>
 )
 
 export default Skills;
