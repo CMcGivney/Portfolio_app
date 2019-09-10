d9149e24ab284afafe1f3ad3b18ba2a66e8f66c5
Chris McGivney <cmmcgivney84@gmail.com>
2019-09-10 12:34:32 -0600

    added image to homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Header, Image, Grid } from 'semantic-ui-react'
 import Alaska from '../images/Alaska.jpg'
+import buckskinGultch from '../images/buckskinGultch.jpg'
 
 const Home = () => (
-  <Grid stackable >
+  <Grid stackable columns="equal">
     <Grid.Row>
        <Grid.Column className="headerBox">
         <Header as="h1" textAlign="center">Chris McGivney</Header>
         <p> Full Stack Web Developer | Ruby on Rails & React | Outdoor Adventure Fanatic</p>
        </Grid.Column>
      </Grid.Row>
-      <Grid.Row column={2}>
+      <Grid.Row>
        <Grid.Column width={8}>
          <Image 
           className="imgBoxHome"
@@ -19,8 +20,8 @@ const Home = () => (
           centered
           />
        </Grid.Column>
-      <Grid.Column width={8}>
-        <div className="aboutMe">
+       <Grid.Column width={8}>
+        <div className="aboutMeHome">
          <Header as="h3">About Me:</Header>
           <p>
             I live in Park City, Utah and have called Utah home for the past 15 years. I grew up in
@@ -38,7 +39,7 @@ const Home = () => (
             contribute to this ever-changing tech landscape.
           </p>
         </div>
-       <div className="aboutMe">
+       <div className="aboutMeHome">
         <Header as="h3">Goals For A Dream Job:</Header>
          <ul>
           <li>Work at a company that I believe in the product, culture and community impact.</li>
@@ -47,26 +48,38 @@ const Home = () => (
           <li>Is it too much to ask for a outdoor company, with a bit of travel?</li>
          </ul>
        </div>
-       <div className="aboutMe">
-        <Header as="h3">My Strategy For Success:</Header>
-          <ul>
-           <li>Work on a personal project everyday.</li>
-           <li>Choose a new tool to learn each week/month.</li>
+       </Grid.Column>
+      </Grid.Row> 
+       <Grid.Row>
+         <Grid.Column>
+          <Image 
+            className="imgBoxHome"
+            src={buckskinGultch} 
+            alt="Two people walking in a slot canyon, Utah."
+            centered
+            />
+         </Grid.Column>
+          <Grid.Column>
+          <div className="aboutMeHome">
+             <Header as="h3">My Strategy For Success:</Header>
             <ul>
-             <li>Currently, mine is GraphQL</li> 
-            </ul>
-          <li>Run through whiteboard problems daily.</li>
-          <li>Spend 30 minutes on a coding problem; solve by using each language I know.</li>
-          <li>Research and contact companies that spark my interests and skill set.</li>
-          <li>Go to meetups and become a part of the Utah community.</li>
-          <li>Contact local leaders and recruiters for quick insight to what they look for in new hires.</li>
-          <li>Read, study and adapt to changes in the skills and tools I use. </li>
-          <li>Stay positive; its a long road, I will get there.</li>
-          <li>Stay true and focused on my goals.</li>
-        </ul>
-      </div>
-    </Grid.Column>
-   </Grid.Row> 
+              <li>Work on a personal project everyday.</li>
+              <li>Choose a new tool to learn each week/month.</li>
+             <ul>
+              <li>Currently, mine is GraphQL</li> 
+             </ul>
+              <li>Run through whiteboard problems daily.</li>
+              <li>Spend 30 minutes on a coding problem; solve by using each language I know.</li>
+              <li>Research and contact companies that spark my interests and skill set.</li>
+              <li>Go to meetups and become a part of the Utah community.</li>
+              <li>Contact local leaders and recruiters for quick insight to what they look for in new hires.</li>
+              <li>Read, study and adapt to changes in the skills and tools I use. </li>
+              <li>Stay positive; its a long road, I will get there.</li>
+              <li>Stay true and focused on my goals.</li>
+           </ul>
+        </div>
+         </Grid.Column>
+       </Grid.Row>
 </Grid>
 )
 
